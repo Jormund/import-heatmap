37d4f44e6e5cb7bde906f0b6150f022354535aa3
Jormund <etiennebotek@gmail.com>
2018-10-31 20:14:08 +0100

    Create import-heatmap.meta.js

---
+++ import-heatmap.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-import-heatmap@Jormund
+// @name           IITC plugin : Import Heatmap
+// @category       Layer
+// @version        0.1.1.20181031.1930
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://raw.githubusercontent.com/Jormund/import-heatmap/master/import-heatmap.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Jormund/import-heatmap/master/import-heatmap.user.js
+// @description    [2018-10-31-1930] Import Heatmap from text
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
